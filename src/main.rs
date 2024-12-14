14130f5fd1a9ba885a5e6b4bce2d7af8690ca778
Ole Magnus Fon Johnsen <me@omfj.no>
2024-12-14 07:02:19 +0100

    Year 2024 Day 14

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,7 @@ fn main() {
         (2024, 12) => y2024::day12::Day12::new(input).run(),
         (2024, 13) => y2024::day13::Day13::new(input).run(),
         (2022, 5) => y2022::day05::Day05::new(input).run(),
+        (2024, 14) => y2024::day14::Day14::new(input).run(),
         _ => println!("No implementation for year {} day {}", year, day),
     }
 
---
+++ src/y2024/day14.rs
@@ -0,0 +1,191 @@
+use crate::utils::AdventDay;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+#[derive(Debug, Clone, Copy)]
+struct Robot {
+    p: (i32, i32),
+    v: (i32, i32),
+}
+
+impl Robot {
+    fn next(&mut self, max_width: i32, max_height: i32) {
+        let (px, py) = self.p;
+        let (vx, vy) = self.v;
+
+        let nx = px + vx;
+        let ny = py + vy;
+
+        let nx = if nx < 0 {
+            max_width + nx
+        } else {
+            nx % max_width
+        };
+
+        let ny = if ny < 0 {
+            max_height + ny
+        } else {
+            ny % max_height
+        };
+
+        self.p = (nx, ny);
+    }
+}
+
+pub struct Day14 {
+    input: String,
+}
+
+impl AdventDay for Day14 {
+    fn new(input: String) -> Self {
+        Self { input }
+    }
+
+    fn part_one(&self) -> String {
+        let mut robots = collect_robots(&self.input);
+        let (max_width, max_height) = get_dimensions(&robots);
+
+        for _ in 0..100 {
+            for robot in robots.iter_mut() {
+                robot.next(max_width, max_height);
+            }
+        }
+
+        let quadrants = collect_robots_in_quadrants(&robots, max_width, max_height);
+        let mut sum = 1;
+        for quadrant in quadrants {
+            sum = sum * quadrant.len();
+        }
+
+        sum.to_string()
+    }
+
+    fn part_two(&self) -> String {
+        let mut robots = collect_robots(&self.input);
+        let (max_width, max_height) = get_dimensions(&robots);
+
+        let mut seconds = 0;
+        loop {
+            seconds += 1;
+
+            let mut positions = HashSet::new();
+
+            for robot in robots.iter_mut() {
+                robot.next(max_width, max_height);
+                positions.insert(robot.p);
+            }
+
+            // All robots are in unique positions
+            if positions.len() == robots.len() {
+                // display_robots(&robots, max_width, max_height);
+                break;
+            }
+        }
+
+        seconds.to_string()
+    }
+}
+
+fn get_dimensions(robots: &[Robot]) -> (i32, i32) {
+    let max_height = robots.iter().map(|r| r.p.1).max().unwrap() + 1;
+    let max_width = robots.iter().map(|r| r.p.0).max().unwrap() + 1;
+
+    (max_width, max_height)
+}
+
+fn collect_robots(input: &str) -> Vec<Robot> {
+    input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split(" ");
+            let p = parts
+                .next()
+                .unwrap()
+                .split("=")
+                .nth(1)
+                .unwrap()
+                .split(",")
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect_tuple()
+                .unwrap();
+            let v = parts
+                .next()
+                .unwrap()
+                .split("=")
+                .nth(1)
+                .unwrap()
+                .split(",")
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect_tuple()
+                .unwrap();
+
+            Robot { p, v }
+        })
+        .collect()
+}
+
+fn collect_robots_in_quadrants(
+    robots: &[Robot],
+    max_width: i32,
+    max_height: i32,
+) -> Vec<Vec<Robot>> {
+    let mut quadrants = vec![Vec::new(); 4];
+
+    for robot in robots {
+        let (x, y) = robot.p;
+
+        if x < max_width / 2 && y < max_height / 2 {
+            quadrants[0].push(*robot);
+        }
+
+        if x > max_width / 2 && y < max_height / 2 {
+            quadrants[1].push(*robot);
+        }
+
+        if x < max_width / 2 && y > max_height / 2 {
+            quadrants[2].push(*robot);
+        }
+
+        if x > max_width / 2 && y > max_height / 2 {
+            quadrants[3].push(*robot);
+        }
+    }
+
+    quadrants
+}
+
+#[allow(dead_code)]
+fn display_robots(robots: &[Robot], max_width: i32, max_height: i32) {
+    for y in 0..max_height {
+        for x in 0..max_width {
+            let is_robot = robots.iter().find(|r| r.p == (x, y)).is_some();
+            let char = if is_robot { '#' } else { '.' };
+            print!("{}", char);
+        }
+        print!("\n");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DATA: &str = r#"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"#;
+
+    #[test]
+    fn part_one() {
+        let day14 = Day14::new(DATA.to_string());
+        assert_eq!(day14.part_one(), "12");
+    }
+}
