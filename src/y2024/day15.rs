94ab5b8af968965fb5e00d13afa59ab950e6ea12
Ole Magnus Fon Johnsen <me@omfj.no>
2024-12-20 04:33:29 +0100

    Fix day 15

--- src/y2024/day15.rs
+++ src/y2024/day15.rs
@@ -1,198 +1,33 @@
-use crate::utils::AdventDay;
-use core::fmt;
+use crate::AdventDay;
 use itertools::Itertools;
 
-#[derive(Debug, Clone)]
-struct Game {
-    board: Vec<Vec<Tile>>,
-}
+type Grid = Vec<Vec<Tile>>;
+type Point = (usize, usize);
 
-impl Game {
-    fn new(board: Vec<Vec<Tile>>) -> Self {
-        Self { board }
-    }
-
-    fn make_wide(&mut self) {
-        let mut wide_board = vec![vec![Tile::Wall; self.board[0].len() * 2]; self.board.len()];
-
-        for (i, row) in self.board.iter().enumerate() {
-            for (j, tile) in row.iter().enumerate() {
-                let j1 = j * 2;
-                let j2 = j1 + 1;
-                match tile {
-                    Tile::Box => {
-                        wide_board[i][j1] = Tile::LeftBox;
-                        wide_board[i][j2] = Tile::RightBox;
-                    }
-                    Tile::Robot => {
-                        wide_board[i][j1] = Tile::Robot;
-                        wide_board[i][j2] = Tile::Empty;
-                    }
-                    _ => {
-                        wide_board[i][j1] = *tile;
-                        wide_board[i][j2] = *tile;
-                    }
-                }
-            }
-        }
-
-        self.board = wide_board;
-    }
-
-    fn score(&self) -> i32 {
-        let mut score = 0;
-
-        for (i, row) in self.board.iter().enumerate() {
-            for (j, tile) in row.iter().enumerate() {
-                if *tile == Tile::Box || *tile == Tile::LeftBox {
-                    score += 100 * i + j;
-                }
-            }
-        }
-
-        score as i32
-    }
-
-    fn find_robot(&self) -> (usize, usize) {
-        for (y, row) in self.board.iter().enumerate() {
-            for (x, tile) in row.iter().enumerate() {
-                if *tile == Tile::Robot {
-                    return (x, y);
-                }
-            }
-        }
-
-        panic!("Robot not found");
-    }
-
-    fn get_next(&self, x: usize, y: usize, direction: Direction) -> (usize, usize) {
-        let (dx, dy) = match direction {
-            Direction::Up => (0, -1),
-            Direction::Down => (0, 1),
-            Direction::Left => (-1, 0),
-            Direction::Right => (1, 0),
-        };
-
-        let next_x = (x as i32 + dx) as usize;
-        let next_y = (y as i32 + dy) as usize;
-
-        (next_x, next_y)
-    }
-
-    fn move_robot(&mut self, direction: Direction) {
-        let (x, y) = self.find_robot();
-        let (next_x, next_y) = self.get_next(x, y, direction);
-        let next_tile = self.board[next_y][next_x];
-
-        match next_tile {
-            Tile::Empty => {
-                self.board[y][x] = Tile::Empty;
-                self.board[next_y][next_x] = Tile::Robot;
-            }
-            Tile::Box => {
-                let offset: usize = self.moveable_tiles(x, y, direction).try_into().unwrap();
-
-                if offset == 0 {
-                    return;
-                }
-
-                for i in 0..offset {
-                    match direction {
-                        Direction::Up => {
-                            self.board[y - offset + i][x] = self.board[y - offset + i + 1][x];
-                            self.board[y - offset + i + 1][x] = Tile::Empty;
-                        }
-                        Direction::Down => {
-                            self.board[y + offset - i][x] = self.board[y + offset - i - 1][x];
-                            self.board[y + offset - i - 1][x] = Tile::Empty;
-                        }
-                        Direction::Left => {
-                            self.board[y][x - offset + i] = self.board[y][x - offset + i + 1];
-                            self.board[y][x - offset + i + 1] = Tile::Empty;
-                        }
-                        Direction::Right => {
-                            self.board[y][x + offset - i] = self.board[y][x + offset - i - 1];
-                            self.board[y][x + offset - i - 1] = Tile::Empty;
-                        }
-                    }
-                }
-            }
-            _ => (),
-        }
-    }
-
-    /// Scans the direction to see if it can move.
-    ///
-    /// @ -> O -> O -> . = 3
-    /// @ -> O -> O -> O -> O -> . = 5
-    /// @ -> O -> # = 0
-    /// @ -> . = 1
-    /// @ -> # = 0
-    ///
-    /// Returns the amount of tiles in the direction that can be moved
-    fn moveable_tiles(&self, x: usize, y: usize, direction: Direction) -> i32 {
-        let mut count = 0;
-        let (mut x, mut y) = (x as i32, y as i32);
-
-        loop {
-            let (next_x, next_y) = self.get_next(x as usize, y as usize, direction);
-            let next_tile = self.board[next_y][next_x];
-
-            match next_tile {
-                Tile::Empty => {
-                    return count + 1;
-                }
-                Tile::Box => {
-                    count += 1;
-                    x = next_x as i32;
-                    y = next_y as i32;
-                }
-                _ => {
-                    return 0;
-                }
-            }
-        }
-    }
-}
-
-impl fmt::Display for Game {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut output = String::new();
-
-        for row in &self.board {
-            for tile in row {
-                let c = match tile {
-                    Tile::Wall => '#',
-                    Tile::Empty => '.',
-                    Tile::Box => 'O',
-                    Tile::Robot => '@',
-                    Tile::LeftBox => '[',
-                    Tile::RightBox => ']',
-                };
-
-                output.push(c);
-            }
-
-            output.push('\n');
-        }
-
-        write!(f, "{}", output)
-    }
-}
-
-type Moves = Vec<Direction>;
-
-#[derive(Clone, Debug, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq)]
 enum Tile {
     Wall,
     Empty,
+    Robot,
     Box,
     LeftBox,
     RightBox,
-    Robot,
 }
 
-#[derive(Clone, Debug, Copy)]
+impl Tile {
+    fn from(c: char) -> Self {
+        match c {
+            '#' => Tile::Wall,
+            '.' => Tile::Empty,
+            'O' => Tile::Box,
+            '@' => Tile::Robot,
+            '[' => Tile::LeftBox,
+            ']' => Tile::RightBox,
+            _ => panic!("Invalid tile: {}", c),
+        }
+    }
+}
+
 enum Direction {
     Up,
     Down,
@@ -201,15 +36,184 @@ enum Direction {
 }
 
 impl Direction {
-    fn from_char(c: char) -> Self {
+    fn from(c: char) -> Self {
         match c {
-            '^' => Self::Up,
-            'v' => Self::Down,
-            '<' => Self::Left,
-            '>' => Self::Right,
-            _ => panic!("Invalid direction"),
+            '^' => Direction::Up,
+            'v' => Direction::Down,
+            '<' => Direction::Left,
+            '>' => Direction::Right,
+            _ => panic!("Invalid direction: {}", c),
         }
     }
+
+    fn next(&self, x: usize, y: usize) -> Point {
+        match self {
+            Direction::Up => (x, y - 1),
+            Direction::Down => (x, y + 1),
+            Direction::Left => (x - 1, y),
+            Direction::Right => (x + 1, y),
+        }
+    }
+}
+
+struct Board {
+    grid: Grid,
+}
+
+impl Board {
+    fn new(grid: Vec<Vec<Tile>>) -> Self {
+        Self { grid }
+    }
+
+    fn find_robot(&self) -> Point {
+        for (y, line) in self.grid.iter().enumerate() {
+            for (x, c) in line.iter().enumerate() {
+                if *c == Tile::Robot {
+                    return (x, y);
+                }
+            }
+        }
+
+        panic!("No robot found");
+    }
+
+    fn make_wide(&mut self) -> Self {
+        let mut grid = vec![];
+        for line in self.grid.iter() {
+            let mut new_line = vec![];
+            for tile in line.iter() {
+                match tile {
+                    Tile::Robot => {
+                        new_line.push(Tile::Robot);
+                        new_line.push(Tile::Empty);
+                    }
+                    Tile::Box => {
+                        new_line.push(Tile::LeftBox);
+                        new_line.push(Tile::RightBox);
+                    }
+                    _ => {
+                        new_line.push(*tile);
+                        new_line.push(*tile);
+                    }
+                }
+            }
+            grid.push(new_line);
+        }
+
+        Self { grid }
+    }
+
+    fn step(&mut self, direction: &Direction) {
+        let (x, y) = self.find_robot();
+        let (nx, ny) = direction.next(x, y);
+        let tile = self.grid[ny][nx];
+
+        match tile {
+            Tile::Wall => (),
+            Tile::Empty => {
+                self.grid[y][x] = Tile::Empty;
+                self.grid[ny][nx] = Tile::Robot;
+            }
+            _ => {
+                self.try_push(nx, ny, direction);
+
+                if self.grid[ny][nx] == Tile::Empty {
+                    self.grid[y][x] = Tile::Empty;
+                    self.grid[ny][nx] = Tile::Robot;
+                }
+            }
+        }
+    }
+
+    fn try_push(&mut self, x: usize, y: usize, direction: &Direction) {
+        match self.push_moves(x, y, direction) {
+            Some(moves) => {
+                let moves = moves.into_iter().unique().collect::<Vec<_>>();
+
+                for (x, y) in moves {
+                    let (nx, ny) = direction.next(x, y);
+                    (self.grid[y][x], self.grid[ny][nx]) = (self.grid[ny][nx], self.grid[y][x]);
+                }
+            }
+            None => (),
+        }
+    }
+
+    fn push_moves(&self, x: usize, y: usize, direction: &Direction) -> Option<Vec<Point>> {
+        let (nx, ny) = direction.next(x, y);
+
+        let connected = match (self.grid[y][x], direction) {
+            (Tile::LeftBox, Direction::Up | Direction::Down) => Some((x + 1, y)),
+            (Tile::RightBox, Direction::Up | Direction::Down) => Some((x - 1, y)),
+            _ => None,
+        };
+
+        let connected_new = connected.map(|(x, y)| direction.next(x, y));
+
+        match (self.grid[ny][nx], connected, connected_new) {
+            (Tile::Wall, _, _) => None,
+            (_, _, Some((x, y))) if self.grid[y][x] == Tile::Wall => None,
+            (Tile::Empty, _, None) => return Some(vec![(x, y)]),
+            (Tile::Empty, Some((cn, cy)), Some((cnx, cny)))
+                if self.grid[cny][cnx] == Tile::Empty =>
+            {
+                Some(vec![(x, y), (cn, cy)])
+            }
+            _ => {
+                let mut all_moves = vec![];
+                if self.grid[ny][nx] != Tile::Empty {
+                    all_moves.extend(self.push_moves(nx, ny, direction)?);
+                }
+                if let Some((other_new_x, other_new_y)) = connected_new {
+                    if self.grid[other_new_y][other_new_x] != Tile::Empty {
+                        all_moves.extend(self.push_moves(other_new_x, other_new_y, direction)?);
+                    }
+                }
+
+                all_moves.push((x, y));
+                if let Some((other_x, other_y)) = connected {
+                    all_moves.push((other_x, other_y));
+                }
+
+                Some(all_moves)
+            }
+        }
+    }
+
+    fn score(&self) -> usize {
+        let mut score = 0;
+
+        for (y, line) in self.grid.iter().enumerate() {
+            for (x, &tile) in line.iter().enumerate() {
+                if tile == Tile::LeftBox || tile == Tile::Box {
+                    score += 100 * y + x;
+                }
+            }
+        }
+
+        score
+    }
+}
+
+fn parse_input(input: &str) -> (Grid, Vec<Direction>) {
+    let (grid, directions) = input.split_once("\n\n").unwrap();
+
+    let grid = grid
+        .lines()
+        .map(|line| line.chars().map(Tile::from).collect::<Vec<Tile>>())
+        .collect::<Vec<_>>();
+
+    let directions = directions
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(Direction::from)
+                .collect::<Vec<Direction>>()
+        })
+        .flatten()
+        .collect::<Vec<_>>();
+
+    (grid, directions)
 }
 
 pub struct Day15 {
@@ -222,63 +226,26 @@ impl AdventDay for Day15 {
     }
 
     fn part_one(&self) -> String {
-        let (mut game, moves) = parse_input(&self.input);
+        let (grid, instructions) = parse_input(&self.input);
+        let mut board = Board::new(grid);
 
-        for direction in moves {
-            game.move_robot(direction);
+        for instruction in instructions {
+            board.step(&instruction);
         }
 
-        game.score().to_string()
+        board.score().to_string()
     }
 
     fn part_two(&self) -> String {
-        let (mut game, moves) = parse_input(&self.input);
-        game.make_wide();
+        let (grid, instructions) = parse_input(&self.input);
+        let mut board = Board::new(grid).make_wide();
 
-        for direction in moves {
-            game.move_robot(direction);
+        for instruction in instructions {
+            board.step(&instruction);
         }
 
-        game.score().to_string()
+        board.score().to_string()
     }
-}
-
-fn parse_input(input: &str) -> (Game, Moves) {
-    let (game, moves) = input.split("\n\n").collect_tuple().unwrap();
-    let game = parse_game(game);
-    let moves = parse_moves(moves);
-
-    (game, moves)
-}
-
-fn parse_game(input: &str) -> Game {
-    let board: Vec<Vec<Tile>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| match c {
-                    '#' => Tile::Wall,
-                    '.' => Tile::Empty,
-                    'O' => Tile::Box,
-                    '@' => Tile::Robot,
-                    '[' => Tile::LeftBox,
-                    ']' => Tile::RightBox,
-                    _ => panic!("Invalid tile"),
-                })
-                .collect()
-        })
-        .collect();
-
-    Game::new(board)
-}
-
-fn parse_moves(input: &str) -> Moves {
-    input
-        .lines()
-        .join("")
-        .chars()
-        .map(Direction::from_char)
-        .collect()
 }
 
 #[cfg(test)]
@@ -309,7 +276,7 @@ mod tests {
 
 <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
 vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
-<>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
 <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
 ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
 ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
@@ -331,7 +298,6 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"#;
     }
 
     #[test]
-    #[ignore = "not finished"]
     fn part_two() {
         let day15 = Day15::new(DATA_2.to_string());
         assert_eq!(day15.part_two(), "9021");
