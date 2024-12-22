3948cd308583a0dc9bbc5149ef78df905074631c
Ole Magnus Fon Johnsen <me@omfj.no>
2024-12-22 19:47:29 +0100

    Year 2024 Day 22

--- src/utils.rs
+++ src/utils.rs
@@ -6,31 +6,26 @@ pub trait AdventDay {
     fn part_one(&self) -> String;
     fn part_two(&self) -> String;
     fn run(&self) {
-        let start_one = Instant::now();
-        let part_one = self.part_one();
-        let elapsed_one = start_one.elapsed();
+        run_part("Part 1", || self.part_one());
+        run_part("Part 2", || self.part_two());
+    }
+}
 
-        if part_one.is_empty() {
-            println!("Part one not implemented");
-        } else {
-            println!("Part one: {}", part_one);
-            println!("Part one took {:?}", elapsed_one);
-        }
+fn run_part(part: &str, f: impl FnOnce() -> String) {
+    let start = Instant::now();
+    let output = f();
+    let elapsed = start.elapsed();
 
-        let start_two = Instant::now();
-        let part_two = self.part_two();
-        let elapsed_two = start_two.elapsed();
-
-        if part_two.is_empty() {
-            println!("Part two not implemented");
-        } else {
-            println!("Part two: {}", part_two);
-            println!("Part two took {:?}", elapsed_two);
-        }
+    if output.is_empty() {
+        println!("{} not implemented", part);
+    } else {
+        println!("{}: {} ({:?})", part, output, elapsed);
     }
 }
 
 pub fn read_input(year: i32, day: i32) -> String {
-    fs::read_to_string(format!("data/inputs/{}/day{:02}.input.txt", year, day))
-        .unwrap_or_else(|_| panic!("Could not read file for year {} day {}", year, day))
+    fs::read_to_string(format!("data/inputs/{}/day{:02}.input.txt", year, day)).expect(&format!(
+        "Could not read file for year {} day {}",
+        year, day
+    ))
 }
--- src/y2024/day22.rs
+++ src/y2024/day22.rs
@@ -1,7 +1,71 @@
 use crate::utils::AdventDay;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+const MODULO: i64 = 16_777_216;
 
 pub struct Day22 {
     input: String,
+}
+
+fn step(number: i64) -> i64 {
+    let mut num = number;
+    num = (num ^ (num * 64)) % MODULO;
+    num = (num ^ (num / 32)) % MODULO;
+    num = (num ^ (num * 2048)) % MODULO;
+    num
+}
+
+fn n_steps(number: i64, n: usize) -> i64 {
+    (0..n).fold(number, |num, _| step(num))
+}
+
+fn parse_input(input: &str) -> Vec<i64> {
+    input.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+fn generate_history(input: &[i64]) -> Vec<Vec<i32>> {
+    // input
+    //     .iter()
+    //     .map(|&num| {
+    //         (0..2000)
+    //             .scan(num, |state, _| {
+    //                 *state = step(*state);
+    //                 Some((*state % 10) as i32)
+    //             })
+    //             .collect()
+    //     })
+    //     .collect()
+    input
+        .iter()
+        .map(|&num| {
+            (0..2000)
+                .scan(num, |state, _| {
+                    *state = step(*state);
+                    Some((*state % 10) as i32)
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn find_best_sequence(history: &[Vec<i32>]) -> i32 {
+    let mut best: HashMap<(i32, i32, i32, i32), i32> = HashMap::new();
+
+    for changes in history {
+        let mut seen = HashMap::new();
+
+        for (&a, &b, &c, &d, &e) in changes.iter().tuple_windows() {
+            let key = (b - a, c - b, d - c, e - d);
+
+            seen.entry(key).or_insert_with(|| {
+                *best.entry(key).or_insert(0) += e;
+                e
+            });
+        }
+    }
+
+    *best.values().max().unwrap_or(&0)
 }
 
 impl AdventDay for Day22 {
@@ -10,13 +74,20 @@ impl AdventDay for Day22 {
     }
 
     fn part_one(&self) -> String {
-        // Solve part 1 here
-        "".to_string()
+        let sum = parse_input(&self.input)
+            .iter()
+            .map(|&secret| n_steps(secret, 2000))
+            .sum::<i64>();
+
+        sum.to_string()
     }
 
     fn part_two(&self) -> String {
-        // Solve part 2 here
-        "".to_string()
+        let input = parse_input(&self.input);
+        let history = generate_history(&input);
+        let best_value = find_best_sequence(&history);
+
+        best_value.to_string()
     }
 }
 
@@ -24,18 +95,25 @@ impl AdventDay for Day22 {
 mod tests {
     use super::*;
 
-    const DATA: &str = "";
+    const DATA_1: &str = r#"1
+10
+100
+2024"#;
+
+    const DATA_2: &str = r#"1
+2
+3
+2024"#;
 
     #[test]
     fn part_one() {
-        let day22 = Day22::new(DATA.to_string());
-        assert_eq!(day22.part_one(), "");
+        let day22 = Day22::new(DATA_1.to_string());
+        assert_eq!(day22.part_one(), "37327623");
     }
 
     #[test]
-    #[ignore = "TODO: Implement me!"]
     fn part_two() {
-        let day22 = Day22::new(DATA.to_string());
-        assert_eq!(day22.part_two(), "");
+        let day22 = Day22::new(DATA_2.to_string());
+        assert_eq!(day22.part_two(), "23");
     }
 }
