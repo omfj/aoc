50132703fe53da9857610f20c82d92f598faafdf
Ole Magnus Fon Johnsen <me@omfj.no>
2024-12-23 15:53:10 +0100

    Year 2024 Day 4

---
+++ src/y2015/day04.rs
@@ -0,0 +1,50 @@
+use crate::utils::AdventDay;
+
+fn find_hash(input: &str, prefix: &str) -> u64 {
+    let mut i = 0;
+
+    loop {
+        let hash = format!("{:x}", md5::compute(format!("{}{}", input, i)));
+        if hash.starts_with(prefix) {
+            return i;
+        }
+
+        i += 1;
+    }
+}
+
+pub struct Day04 {
+    input: String,
+}
+
+impl AdventDay for Day04 {
+    fn new(input: String) -> Self {
+        Self { input }
+    }
+
+    fn part_one(&self) -> String {
+        find_hash(&self.input, "00000").to_string()
+    }
+
+    fn part_two(&self) -> String {
+        find_hash(&self.input, "000000").to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        let day04 = Day04::new("abcdef".to_string());
+        assert_eq!(day04.part_one(), "609043".to_string());
+    }
+
+    #[test]
+    #[ignore = "TODO: Implement me!"]
+    fn part_two() {
+        let day04 = Day04::new("".to_string());
+        assert_eq!(day04.part_two(), "");
+    }
+}
